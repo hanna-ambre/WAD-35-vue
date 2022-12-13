e0a7611c8da526bdc99ee4c4ebf76280f093d504
Hanna Saskia Ambre <hanna.s.ambre@gmail.com>
2022-12-13 10:10:17 +0200

    authenticating signup and login added

--- src/router/index.js
+++ src/router/index.js
@@ -1,18 +1,35 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import mainView from '../../../WAD-35-vue/src/views/mainView.vue'
 import signupView from '../../../WAD-35-vue/src/views/signupView.vue'
+import loginView from '../../../WAD-35-vue/src/views/loginView.vue'
 import newPostView from '../../../WAD-35-vue/src/views/newPost.vue'
 import contactUsView from '../../../WAD-35-vue/src/views/contactUs.vue'
+import auth from "../auth";
+
+
 
 const routes = [{
     path: '/',
     name: 'main',
-    component: mainView
+    component: mainView,
+    beforeEnter: async(to, from, next) => {
+        let authResult = await auth.authenticated();
+        if (!authResult) {
+            next('/login')
+        } else {
+            next();
+        }
+    }
     },
     {
         path: '/signup',
         name: 'signup',
         component: signupView
+    },
+    {
+        path: '/login',
+        name: 'login',
+        component: loginView
     },
     {
         path: '/newPost',
